aff564e395f0df4cbf4b3711ec973ca9e4c7830e
Linda MacPhee-Cobb <timestocome@gmail.com>
2017-08-30 17:49:44 -0500

    Run image capture loop forever, save as numpy array instead of as image

--- LoadImages.py
+++ LoadImages.py
@@ -1,4 +1,8 @@
-# start grabbing video from camera and saving to disk for use in other code
+
+# https://github.com/timestocome/RaspberryPi
+
+# start grabbing video from RaspberryPi camera and saving to disk as numpy array
+# if i > 5 saves 7 per loop (0...6)
 
 
 # https://www.raspberrypi.org/learning/getting-started-with-picamera/worksheet/
@@ -6,6 +10,7 @@
 
 from picamera import PiCamera
 from time import sleep
+import numpy as np
 
 
 
@@ -13,29 +18,22 @@ from time import sleep
 camera = PiCamera()
 camera.rotation = 180      # rotate if image not upright
 
-
 # default is 1920 x 1080  drop it way down for TF
-camera.resolution = (180, 180)
-camera.framerate = 5
-print(camera.resolution)   
+height = 192
+width = 192
+camera.resolution = (width, height)
+camera.framerate = 1
 
+i = 0
 
-# loop capture a series of images
-for i in range(5):
-    sleep(1)
-    camera.capture('/home/pi/videoFeed/image%s.jpg' % i)
-
-
-
-
-
-'''
-# test record video
-camera.start_preview()
-camera.start_recording('/home/pi/videoFeed.h264')
-
-sleep(5)
-
-camera.stop_recording()
-camera.stop_preview()
-'''
+while(True):
+    if i > 5: i = 0
+    else: i+=1
+    filename = '/home/pi/videoFeed/image%s.npy' % i
+    
+    output = np.empty((width, height, 3 ), dtype=np.uint8)
+    camera.capture(output, 'rgb')
+    np.save(filename, output)
+                      
+                      
+      
