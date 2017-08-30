ad38586f4d6833f12409f36cf6b66245ce14dc59
Linda MacPhee-Cobb <timestocome@gmail.com>
2017-08-30 17:05:50 -0500

    use to capture images to use in TF image id

---
+++ LoadImages.py
@@ -0,0 +1,37 @@
+# start grabbing video from camera
+
+from picamera import PiCamera
+from time import sleep
+
+
+
+# init
+camera = PiCamera()
+camera.rotation = 180      # rotate if image not upright
+
+
+# default is 1920 x 1080  drop it way down for TF
+camera.resolution = (180, 180)
+camera.framerate = 5
+print(camera.resolution)   
+
+
+# loop capture a series of images
+for i in range(5):
+    sleep(1)
+    camera.capture('/home/pi/videoFeed/image%s.jpg' % i)
+
+
+
+
+
+'''
+# test record video
+camera.start_preview()
+camera.start_recording('/home/pi/videoFeed.h264')
+
+sleep(5)
+
+camera.stop_recording()
+camera.stop_preview()
+'''
