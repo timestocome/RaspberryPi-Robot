3bf7df468e7d8363f2c6fede65f43597c63b2ef3
Linda MacPhee-Cobb <timestocome@gmail.com>
2017-09-13 21:18:28 -0500

    added a few more functions

--- test hardware scripts/testWheels.py
+++ test hardware scripts/testWheels.py
@@ -1,11 +1,14 @@
 
+
 # http://github.com/timestocome
 
 
-
-# do not hook up wheels to these pins unless you want can in motion on boot
 # pins 3, 5, 7, 24, 29, 31
 # gpio 0-8 are on by default on boot
+
+
+# more info
+# http://www.mcmanis.com/chuck/robotics/tutorial/h-bridge/
 
 
 
@@ -77,23 +80,67 @@ def reverse_turn_left(t):
     sleep(t)
     gpio.output(38, gpio.LOW)
     
+ 
+
+def hard_right(t=1.):
+    gpio.output(40, gpio.HIGH)
+    gpio.output(33, gpio.HIGH)
+    
+    sleep(t)
+    gpio.output(40, gpio.LOW)
+    gpio.output(33, gpio.LOW)
+    
 
 
-
-
+def hard_left(t=1.):
+    gpio.output(35, gpio.HIGH)
+    gpio.output(38, gpio.HIGH)
+    
+    sleep(t)
+    gpio.output(35, gpio.LOW)
+    gpio.output(38, gpio.LOW)
+      
+    
+def stop1(t=1):
+    gpio.output(40, gpio.HIGH)
+    gpio.output(38, gpio.HIGH)
+    
+    sleep(t)
+    gpio.output(40, gpio.LOW)
+    gpio.output(38, gpio.LOW)
+      
+    
+def stop2(t=1):
+    gpio.output(35, gpio.HIGH)
+    gpio.output(33, gpio.HIGH)
+    
+    sleep(t)
+    gpio.output(35, gpio.LOW)
+    gpio.output(33, gpio.LOW)
+      
+  
 
 # test
+
+
+go_forward(2.)
+go_backward(2.)
+
+stop1(1)
+
 reverse_turn_right(2.)
 reverse_turn_left(2.)
 
 turn_left(2.)
 turn_right(2.)
 
-go_forward(2.)
-go_backward(2.)
+stop2(1)
+
+hard_left(2.)
+hard_right(2.)
+
 
 
 # clean up
 gpio.cleanup()
-
 
