1fa9090fe8fbc3ca16e566a545837a69a901d39d
Linda MacPhee-Cobb <timestocome@gmail.com>
2017-09-28 15:41:56 -0500

    further streamlined code

--- ObjectDetection/ObjectDetector.py
+++ ObjectDetection/ObjectDetector.py
@@ -13,14 +13,11 @@ import sys
 import tensorflow as tf
 
 from collections import defaultdict
-from io import StringIO
+
 from PIL import Image
 
 from utils import label_map_util
 
-
-# env
-sys.path.append("..")
 
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
 PATH_TO_CKPT = 'ssd_mobilenet_v1_coco_11_06_2017'+ '/frozen_inference_graph.pb'
@@ -57,16 +54,6 @@ def load_image_into_numpy_array(image):
   return np.array(image.getdata()).reshape((im_height, im_width, 3)).astype(np.uint8)
 
 
-# test detection
-# For the sake of simplicity we will use only 3 images:
-# image1.jpg
-# image2.jpg
-# image3.jpg
-# If you want to test the code with your images, just add path to the images to the TEST_IMAGE_PATHS.
-PATH_TO_TEST_IMAGES_DIR = 'test_images'
-TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(1, 4) ]
-
-
 
 with detection_graph.as_default():
     
@@ -78,34 +65,30 @@ with detection_graph.as_default():
     # Each box represents a part of the image where a particular object was detected.
     detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
     
-    # Each score represent how level of confidence for each of the objects.
-    # Score is shown on the result image, together with the class label.
-    
+    # Each score represent how level of confidence for each of the objects.    
     detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
     detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
     num_detections = detection_graph.get_tensor_by_name('num_detections:0')
     
-    for image_path in TEST_IMAGE_PATHS:
-    
-      image = Image.open(image_path)
+    # next to do is to load this via pi camera as numpy array 
+    image = Image.open('camera_images/image.jpg')
     
     # the array based representation of the image will be used later in order to prepare the
       # result image with boxes and labels on it.
-      image_np = load_image_into_numpy_array(image)
+    image_np = load_image_into_numpy_array(image)
     
     # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-      image_np_expanded = np.expand_dims(image_np, axis=0)
+    image_np_expanded = np.expand_dims(image_np, axis=0)
     
     # Actual detection.
-      (boxes, scores, classes, num) = sess.run(
+    (boxes, scores, classes, num) = sess.run(
           [detection_boxes, detection_scores, detection_classes, num_detections],
           feed_dict={image_tensor: image_np_expanded})
      
      # Print names and locations of first few items with confidence > 80%
-      for s in range(5):
+     # to do - using this, + ultrasonic sensor locate cat position relative to robot
+    for s in range(5):
           if scores[0, s] > .80:
-              print('--------------------------')
-              print(boxes.shape, boxes[0, s])
-              print("object", category_index.get(int(classes[0, s])).get('name'))
+              print(category_index.get(int(classes[0, s])).get('name'), boxes[0,s])
       
     
