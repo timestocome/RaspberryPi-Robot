393798577ddd853c0c47cf10f599c5227e4706b5
Linda MacPhee-Cobb <timestocome@gmail.com>
2017-10-07 12:03:05 -0500

    convert findCats script to class

--- catID/findCats.py
+++ catID/findCats.py
@@ -8,133 +8,128 @@
 
 
 
-import sys
 import numpy as np
 import tensorflow as tf
-
-import numpy as np
 import picamera
-import time
-
-import datetime
-
-print("Start ", datetime.datetime.now())
-
-# set up constants
-height = 128
-width = 128
-input_mean = 0
-input_std = 255
-
-
-# setup graph
-# this is the saved graph that was trained on a desktop
-model_file = "output_graph.pb"
-label_file = "output_labels.txt"
-input_layer = "input"
-output_layer = "final_result"
-
-
-# setup camera
-camera = picamera.PiCamera()
-camera.resolution = (width, height)
-camera.vflip = True
 
 
 
 
-
-
-
-def load_graph(model_file):
+class findCats(object):
     
-  graph = tf.Graph()
-  graph_def = tf.GraphDef()
+    def __init__(self):
 
-  with open(model_file, "rb") as f:
-    graph_def.ParseFromString(f.read())
-
-  with graph.as_default():
-    tf.import_graph_def(graph_def)
-
-  return graph
+        # set up constants
+        self.height = 128
+        self.width = 128
+        self.input_mean = 0
+        self.input_std = 255
 
 
+        # setup graph
+        # this is the saved graph that was trained on a desktop
+        self.model_file = "output_graph.pb"
+        self.label_file = "output_labels.txt"
+        self.input_layer_name = "import/input"
+        self.output_layer_name = "import/final_result"
 
 
-def load_labels(label_file):
+        # setup camera
+        camera = picamera.PiCamera()
+        camera.resolution = (self.width, self.height)
+        camera.vflip = True
+        self.camera = camera
+
+
+
+        # load saved, trained nn    
+        graph = tf.Graph()
+        graph_def = tf.GraphDef()
+
+        with open(self.model_file, "rb") as f:
+            graph_def.ParseFromString(f.read())
+
+        with graph.as_default():
+            tf.import_graph_def(graph_def)
+
+        self.graph = graph
+
+
+        # load labels    
+        label = []
+        proto_as_ascii_lines = tf.gfile.GFile(self.label_file).readlines()
+  
+        for l in proto_as_ascii_lines:
+            label.append(l.rstrip())
+  
+        self.labels = label
+
+
+        # create session
+        self.sess = tf.Session(graph=self.graph)
+
     
-  label = []
-  proto_as_ascii_lines = tf.gfile.GFile(label_file).readlines()
-  
-  for l in proto_as_ascii_lines:
-    label.append(l.rstrip())
-  
-  return label
 
 
-
-
-
-def capture_image():
+    def capture_image(self):
     
-  image = np.empty((width, height, 3), dtype=np.uint8)
-  camera.capture(image, 'rgb')
+      image = np.empty((self.width, self.height, 3), dtype=np.uint8)
+      self.camera.capture(image, 'rgb')
   
-  float_caster = tf.cast(image, tf.float32)
-  dims_expander = tf.expand_dims(float_caster, 0);
-  resized = tf.image.resize_bilinear(dims_expander, [height, width])
+      float_caster = tf.cast(image, tf.float32)
+      dims_expander = tf.expand_dims(float_caster, 0);
+      resized = tf.image.resize_bilinear(dims_expander, [self.height, self.width])
   
-  normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
-  sess = tf.Session()
-  result = sess.run(normalized)
+      normalized = tf.divide(tf.subtract(resized, [self.input_mean]), [self.input_std])
+      sess = tf.Session()
+      result = sess.run(normalized)
 
-  return result
-
-
-
-
- 
-# load graph 
-graph = load_graph(model_file)
-labels = load_labels(label_file)
-sess = tf.Session(graph=graph)
-
-input_name = "import/" + input_layer
-output_name = "import/" + output_layer
-
-print('Graph loaded', datetime.datetime.now())
+      return result
 
 
 
-# loop forever
-counter = 0
+    def run_graph(self):
+
+        # take photo
+        t = self.capture_image()
+  
+        # see if Min, Merlin or no cat in photo
+        input_operation = self.graph.get_operation_by_name(self.input_layer_name);
+        output_operation = self.graph.get_operation_by_name(self.output_layer_name);
+
+        results = self.sess.run(output_operation.outputs[0], {input_operation.outputs[0]: t})
+        results = np.squeeze(results)
+
+        top_k = results.argsort()[-3:][::-1]
+  
+        # print results
+        #for i in top_k:
+        #    print(self.labels[i], results[i])
+        
+        found = []
+        for i in top_k:
+            found.append((self.labels[i], results[i]))
+        return found
+        
+    
+    
+    
+    def cleanup(self):
+        self.sess.close()
+
+
+
+
+##################################################
+# run graph in loop
+#################################################
+loop = findCats()
 
 while(True):
-  
-  # take photo
-  t = capture_image()
-  
-  # see if Min, Merlin or no cat in photo
-  input_operation = graph.get_operation_by_name(input_name);
-  output_operation = graph.get_operation_by_name(output_name);
+    found_cat = loop.run_graph()
+    print(found_cat)
+    print(found_cat[0][0], found_cat[0][1])
 
-  results = sess.run(output_operation.outputs[0], {input_operation.outputs[0]: t})
-  results = np.squeeze(results)
-
-  top_k = results.argsort()[-3:][::-1]
-  
-  # print results
-  for i in top_k:
-    print(labels[i], results[i])
-    
-  counter += 1
-  print("next image", counter, datetime.datetime.now())
-  
-  
-  
-sess.close()
-
-
+loop.cleanup()
 
   
